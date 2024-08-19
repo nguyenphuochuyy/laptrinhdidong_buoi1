5184940e6e4c3ee8a1829f325ef355dab3a33d65
H51M14\Student <Student@gmail.com>
2024-08-19 13:59:42 +0700

    [#2] cau2.2

--- Bai-1/index.js
+++ Bai-1/index.js
@@ -9,7 +9,9 @@ var johnh = 1.95;
 
 var BMI1 = Bmi(markw, markh)
 var BMI2 = Bmi(johnw, johnh)
+
 var markHigherBMI = BMI1 > BMI2;
 console.log(markHigherBMI);
 // bài 2
-BMI1 > BMI2 ? console.log("Mark's BMI is higher than John's !") : console.log("John's BMI is higher than Marks's !");;
+BMI1 > BMI2 ? console.log("1.Mark's BMI is higher than John's !") : console.log("1.John's BMI is higher than Marks's !");
+BMI1 > BMI2 ? console.log(`1.Mark's BMI(${BMI1.toFixed(0)}) is higher than John's(${BMI2.toFixed(0)}) !`) : console.log(`1.John's BMI(${BMI1.toFixed(0)}) is higher than Marks's(${BMI2.toFixed(0)}) !`);
